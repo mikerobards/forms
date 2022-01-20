c225274b66658391f51e6ff09ad29374792468c2
Michael Robards <mikerobards@icloud.com>
2022-01-20 16:46:26 -0500

    add select drop down

--- src/Form.js
+++ src/Form.js
@@ -8,10 +8,11 @@ export default function Form() {
             email: "",
             comments: "",
             isFriendly: true,
-            employment: ""
+            employment: "",
+            favColor: ""
         })
 
-    console.log(formData.employment)
+    console.log(formData.favColor)
 
 
     function handleChange(event) {
@@ -98,8 +99,26 @@ export default function Form() {
                 />
                 <label htmlFor="full-time">Full-time</label>
                 <br />
-
             </fieldset>
+
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+
+            >
+                <option value="">--Choose--</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
         </form>
     )
 }
